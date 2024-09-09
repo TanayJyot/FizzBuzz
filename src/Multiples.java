418b636884df464c620edcf249ab0c2df0a9799d
Tanayjyot <tjsinghchawla@gmail.com>
2024-09-09 18:18:41 -0400

    Well well well

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+
 public class Multiples {
     public static void main(String[] args) {
         int i = 1;
         int counter3 = 0;
+        ArrayList<Integer> multiples3 = new ArrayList<>();
         while (3*i < 1000){
+            multiples3.add(3 * i);
             counter3 = counter3 + 1;
             i++;
         }
         int j = 1;
         int counter5 = 0;
         while (5*j < 1000){
-            counter5 = counter5 + 1;
+            if (!multiples3.contains(5*j)){
+                counter5 = counter5 + 1;
+            }
             j++;
         }
         System.out.println(counter5 + counter3);
